d5d0f325d331bcd2a8788da918f926c825c6199d
stujfiter <korey.earl@gmail.com>
2016-10-18 07:01:03 -0500

    Start and stop and Instance

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
-aws ec2 run-instances --image-id ami-d732f0b7 --key-name Adam --instance-type t2.micro
+aws ec2 create-security-group --group-name my-sg --description "My Security Group"
+aws ec2 authorize-security-group-ingress --group-name my-sg --protocol tcp --port 22 --cidr 0.0.0.0/0
+
+INSTANCE_ID=$(aws ec2 run-instances --image-id ami-d732f0b7 --key-name Adam --instance-type t2.micro --security-groups my-sg | jq -r '.Instances[0].InstanceId')
+
+aws ec2 create-tags --resources $INSTANCE_ID --tags Key=Name,Value=MyInstance
+
+aws ec2 describe-instances --filters "Name=tag-value,Values=MyInstance" | jq -r '.Reservations[0].Instances[0].PublicIpAddress'
--- stop.sh
+++ stop.sh
@@ -1,3 +1,17 @@
 #! /bin/bash
 
-aws ec2 terminate-instances --instance-id i-e2f3e6fa
+INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag-value,Values=MyInstance" | jq -r ".Reservations[0].Instances[0].InstanceId")
+
+STATE=$(aws ec2 describe-instances --instance-id $INSTANCE_ID | jq -r '.Reservations[0].Instances[0].State.Name')
+
+if [ "$STATE" = "running" ]; then
+    aws ec2 terminate-instances --instance-id $INSTANCE_ID
+    until [ "$STATE" = "terminated" ]; do
+        STATE=$(aws ec2 describe-instances --instance-id $INSTANCE_ID | jq -r '.Reservations[0].Instances[0].State.Name')
+    done
+else
+    echo "$INSTANCE_ID: $STATE.  Instance must be in running state to terminate"
+fi
+
+aws ec2 delete-tags --resources $INSTANCE_ID
+aws ec2 delete-security-group --group-name my-sg
