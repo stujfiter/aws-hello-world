5607176f33bbbd9f5e7e24a5a3e296434ea67afe
stujfiter <korey.earl@gmail.com>
2016-11-18 13:47:33 -0600

    added IdentityOnly=true to ssh options so too many authentication failures message is not seen

--- connect.sh
+++ connect.sh
@@ -2,4 +2,4 @@
 
 
 IP_ADDRESS=$(aws ec2 describe-instances --filter "Name=tag-value,Values=MyInstance" | jq -r '.Reservations[0].Instances[0].PublicIpAddress')
-ssh -i adam.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ubuntu@$IP_ADDRESS
+ssh -i adam.pem -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ubuntu@$IP_ADDRESS
--- provision.sh
+++ provision.sh
@@ -1,7 +1,11 @@
 
 
 IP_ADDRESS=$(aws ec2 describe-instances --filter "Name=tag-value,Values=MyInstance" | jq -r '.Reservations[0].Instances[0].PublicIpAddress')
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i adam.pem ubuntu@$IP_ADDRESS /bin/bash << EOF
+ssh -o IdentitiesOnly=yes \
+    -o UserKnownHostsFile=/dev/null \
+    -o StrictHostKeyChecking=no \
+    -i adam.pem ubuntu@$IP_ADDRESS \
+    /bin/bash << EOF
     echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee -a /etc/apt/sources.list.d/docker.list;
     sudo apt-get update;
     sudo apt-get install docker-engine -y --force-yes;
